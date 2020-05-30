bb378b0b9ea9c1e9aeff10919fe530b98e3195f2
birdsea <15532048+birdsea@users.noreply.github.com>
2020-05-30 18:17:03 +0900

    テストプロジェクトを追加

---
+++ Diary-Sample-Test/Controllers/MenuControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Diary_Sample_Test.Controllers
+{
+    public class MenuControllerTest
+    {
+        [Fact]
+        public void IndexTest()
+        {
+
+        }
+    }
+}
