fa45dc6958f888e6e6a9ed25f5dde8531868a2c2
birdsea <15532048+birdsea@users.noreply.github.com>
2020-06-01 21:50:01 +0900

    MenuControllerTest-IndexTest追加

--- Diary-Sample-Test/Controllers/MenuControllerTest.cs
+++ Diary-Sample-Test/Controllers/MenuControllerTest.cs
@@ -1,3 +1,16 @@
+// -----------------------------------------------------------------------
+// <copyright file="MenuControllerTest.cs" company="1-system-group">
+// Copyright (c) 1-system-group. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+using Diary_Sample.Controllers;
+using Diary_Sample.Models;
+using Diary_Sample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace Diary_Sample_Test.Controllers
@@ -7,7 +20,23 @@ namespace Diary_Sample_Test.Controllers
         [Fact]
         public void IndexTest()
         {
+            // Arrange
+            var mockLogger = new Mock<ILogger<MenuController>>();
+            var mockService = new Mock<IMenuService>();
+            mockService.Setup(x => x.Index(null)).Returns(new MenuViewModel());
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
 
+            var controller = new MenuController(mockLogger.Object, mockService.Object);
+            controller.TempData = tempData;
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<MenuViewModel>(viewResult.ViewData.Model);
+            controller.Dispose();
         }
     }
 }
