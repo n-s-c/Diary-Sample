f26a78fd07f7941595fb42d47ce590ee7ac33560
birdsea <15532048+birdsea@users.noreply.github.com>
2020-06-03 22:58:53 +0900

    MenuControllerTestのテストケース追加

--- Diary-Sample-Test/Controllers/MenuControllerTest.cs
+++ Diary-Sample-Test/Controllers/MenuControllerTest.cs
@@ -17,25 +17,64 @@ namespace Diary_Sample_Test.Controllers
 {
     public class MenuControllerTest
     {
+        private Mock<ILogger<MenuController>> mockLogger;
+        private Mock<IMenuService> mockService;
+        private DefaultHttpContext httpContext;
+        private TempDataDictionary tempData;
+
+        public MenuControllerTest()
+        {
+            mockLogger = new Mock<ILogger<MenuController>>();
+            mockService = new Mock<IMenuService>();
+            httpContext = new DefaultHttpContext();
+            mockService.Setup(x => x.Index(null)).Returns(new MenuViewModel());
+            mockService.Setup(x => x.Paging(1)).Returns(new MenuViewModel());
+            tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+        }
         [Fact]
         public void IndexTest()
         {
-            // Arrange
-            var mockLogger = new Mock<ILogger<MenuController>>();
-            var mockService = new Mock<IMenuService>();
-            mockService.Setup(x => x.Index(null)).Returns(new MenuViewModel());
-            var httpContext = new DefaultHttpContext();
-            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
-
             var controller = new MenuController(mockLogger.Object, mockService.Object);
             controller.TempData = tempData;
 
-            // Act
+            // Indexメソッドのテスト
             var result = controller.Index();
 
-            // Assert
+            // 返却値の型を確認
             var viewResult = Assert.IsType<ViewResult>(result);
             var model = Assert.IsType<MenuViewModel>(viewResult.ViewData.Model);
+
+            controller.Dispose();
+        }
+        [Fact]
+        public void PagingTest()
+        {
+            var controller = new MenuController(mockLogger.Object, mockService.Object);
+            controller.TempData = tempData;
+
+            // Pagingメソッドのテスト
+            var result = controller.Paging(1);
+
+            // 返却値の型を確認
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<MenuViewModel>(viewResult.ViewData.Model);
+
+            controller.Dispose();
+        }
+        [Fact]
+        public void NewEntryTest()
+        {
+            var controller = new MenuController(mockLogger.Object, mockService.Object);
+            controller.TempData = tempData;
+
+            // NewEntryメソッドのテスト
+            var result = controller.NewEntry();
+
+            // 遷移先の確認
+            var actionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("index", actionResult.ActionName);
+            Assert.Equal("Create", actionResult.ControllerName);
+
             controller.Dispose();
         }
     }
