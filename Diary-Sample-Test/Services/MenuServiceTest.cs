1e54fc3d8de76d8f47bfe995498e59cde7b34833
birdsea <15532048+birdsea@users.noreply.github.com>
2020-06-07 10:08:23 +0900

    MenuServiceTestの追加

--- Diary-Sample-Test/Services/MenuServiceTest.cs
+++ Diary-Sample-Test/Services/MenuServiceTest.cs
@@ -3,18 +3,11 @@
 // Copyright (c) 1-system-group. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Diary_Sample.Controllers;
 using Diary_Sample.Entities;
 using Diary_Sample.Models;
 using Diary_Sample.Repositories;
 using Diary_Sample.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
@@ -23,9 +16,31 @@ namespace Diary_Sample_Test.Services
 {
     public class MenuServiceTest
     {
+        private Mock<ILogger<MenuService>> mockLogger;
+        private Mock<IDiaryRepository> mockRepository;
+        private MenuService service;
+        public MenuServiceTest()
+        {
+            mockLogger = new Mock<ILogger<MenuService>>();
+            mockRepository = new Mock<IDiaryRepository>();
+            service = new MenuService(mockLogger.Object, mockRepository.Object);
+        }
         [Fact]
         public void IndexTest()
         {
+            // Diaryが0件の場合
+            mockRepository.Setup(x => x.read(1, MenuViewModel.PageCount)).Returns(new List<Diary>());
+            mockRepository.Setup(x => x.readCount()).Returns(0);
+
+            // Indexメソッドのテスト
+            var result = service.Index("test");
+
+            // 返却値の確認
+            var model = Assert.IsType<MenuViewModel>(result);
+            Assert.Equal(0, model.TotalNumber);
+            Assert.Equal(0, model.TotalPageNumber);
+            Assert.Equal("test", model.Notification);
+            Assert.Empty(model.DiaryList);
         }
     }
 }
